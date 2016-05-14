473232ba84737537a521d5318ad753d2636ca5c0
Harshdeep Dhatt <09.harshdeep@gmail.com>
2016-05-14 15:14:27 -0600

    src: events: Fix race condition in modifycontrolcpsr
    There is a following race condition.
    - process A is writing to a led register
    - process A switches to SVC mode
    - It pushes all its registers onto the SVC stack
    - SVC sp is now say at address p
    - It pushes r1 and the return address onto p-1 and p-2 and resets the SVC stack to p
    - Switches to SYS mode with IRQ on
    
    Now at this point timer interrupt schedules process B
    
    - process B is also writing to led register
    - process B switches to SVC mode
    - It pushes all registers onto SVC stack at p, overwriting
      r1 and lr of process A
    
    Now when timer interrupt schedules process A
    - it tries to restore the return address which was corrupted

--- src/events/cpsr.s
+++ src/events/cpsr.s
@@ -1,23 +1,27 @@
 .text
 .code 32
 .global modifyControlCPSR
-
+ADDR_TEMP_STACK:
+	.word	0x80090000
 
 modifyControlCPSR:
+	LDR	r11, =ADDR_TEMP_STACK
+	LDR	r11, [r11]
+	STMFD	r11!, {r1, lr}	/* push on the temporary stack */
+	MOV	r10, r11
+	LDR	r11, =ADDR_TEMP_STACK
+	STR	r10, [r11]	/* store the temporary stack pointer to memory */
 
+	MRS	r1,CPSR
+	BIC	r1,r1,#0xff
+	ORR	r1,r1,r0
+	MSR	CPSR_c,r1
 
-     STMFD   r13!,{r3}
-     MOV     r3,r13	
-     ADD     r13,r13,#4      
-    STMFD   r3!,{r1,lr}
-
-     MRS     r1,CPSR
-     BIC     r1,r1,#0xff
-     ORR     r1,r1,r0
-     MSR     CPSR_c,r1
-
-
-     LDMFD   r3!,{r1,lr}
-     LDMFD   r3,{r3}
-     MOV     pc,lr
-     .end
+	LDR	r11, =ADDR_TEMP_STACK	/* restore the stack pointer in r11 */
+	LDR	r11, [r11]
+	LDMFD	r11!, {r1, lr}	/* pop the registers on the temporary stack */
+	MOV	r10, r11
+	LDR	r11, =ADDR_TEMP_STACK
+	STR	r10, [r11]	/* store the temporary stack pointer to memory */
+	MOV	pc,lr
+	.end
